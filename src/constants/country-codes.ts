a435332a7a9ec563447a5664212e62b423d56d3f
Krish Acme <acmekrish@gmail.com>
2025-09-24 14:31:54 +0100

    removed plus

--- src/constants/country-codes.ts
+++ src/constants/country-codes.ts
@@ -9,43 +9,43 @@ export const countryCodes: CountryCode[] = [
     {
         name: "Nigeria",
         code: "NG",
-        dialCode: "+234",
+        dialCode: "234",
         flag: "🇳🇬"
     },
     {
         name: "Ghana",
         code: "GH",
-        dialCode: "+233",
+        dialCode: "233",
         flag: "🇬🇭"
     },
     {
         name: "Germany",
         code: "DE",
-        dialCode: "+49",
+        dialCode: "49",
         flag: "🇩🇪"
     },
     {
         name: "Egypt",
         code: "EG",
-        dialCode: "+20",
+        dialCode: "20",
         flag: "🇪🇬"
     },
     {
         name: "South Africa",
         code: "ZA",
-        dialCode: "+27",
+        dialCode: "27",
         flag: "🇿🇦"
     },
     {
         name: "United States",
         code: "US",
-        dialCode: "+1",
+        dialCode: "1",
         flag: "🇺🇸"
     },
     {
         name: "United Kingdom",
         code: "GB",
-        dialCode: "+44",
+        dialCode: "44",
         flag: "🇬🇧"
     }
 ];
